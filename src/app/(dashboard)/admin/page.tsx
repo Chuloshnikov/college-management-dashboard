31ca0ee5ae1388e0581c8e6225f5a99f0aebd418
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-10-26 15:03:54 +0300

    calendar added

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,12 +1,13 @@
 import AttendanceChart from '@/components/AttendanceChart'
 import CountChart from '@/components/CountChart'
+import EventCalendar from '@/components/EventCalendar'
 import FinanceChart from '@/components/FinanceChart'
 import UserCard from '@/components/UserCard'
 import React from 'react'
 
 export default function AdminPage() {
   return (
-    <div className='p-4 flex gap-4 flex-col md;flex-row'>
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
       {/*LEFT*/}
       <div className='w-full lg:w-2/3 flex flex-col gap-8'>
           {/*USER CARDS*/}
@@ -20,7 +21,7 @@ export default function AdminPage() {
           <div className='flex gap-4 flex-col lg:flex-row'>
             {/* COUNT CHART */}
           <div className='w-full lg:w-1/3 h-[450px]'>
-            <CountChart />
+              <CountChart />
           </div>
             {/*ATTENDANCE CHART*/}
             <div className='w-full lg:w-2/3 h-[450px]'>
@@ -28,13 +29,14 @@ export default function AdminPage() {
             </div>
           </div>
            {/* BOTTOM CHART */}
-          <div className='w-full h-[500px]'>
+          <div className='w-full h-[500px]'>  
               <FinanceChart/>
           </div>
       </div>
       {/*RIGHT*/}
-      <div className='w-full lg:w-1/3'>
-      </div>
+        <div className='w-full lg:w-1/3 flex flex-col gap-8'>
+              <EventCalendar/>
+        </div>
     </div>
   )
 }
