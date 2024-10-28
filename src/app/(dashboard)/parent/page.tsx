e6d24e2bf72437c3c36d7e8696b4e1390f9c6099
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-10-28 16:21:52 +0200

    +

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,9 +1,23 @@
+"use client"
+
+import Announcements from '@/components/Announcements'
+import BigCalendar from '@/components/BigCalendar'
 import React from 'react'
 
 export default function ParentPage() {
   return (
-    <div>
-        Parent
+    <div className='p-4 flex gap-4 flex-col xl:flex-row'>
+      {/*LEFT*/}
+      <div className='w-full xl:w-2/3'>
+         <div className='h-full bg-white p-4 rounded-md'>
+            <h2 className='text-xl font-semibold'>Schedule (John Doe)</h2>
+            <BigCalendar/>
+         </div>
+      </div>
+      {/*RIGHT*/}
+        <div className='w-full xl:w-1/3'>
+          <Announcements/>
+        </div>
     </div>
   )
 }
