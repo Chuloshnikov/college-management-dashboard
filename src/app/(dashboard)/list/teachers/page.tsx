510f1ef2a648d4e3e11b8c99de86922679094f96
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-10-29 18:02:37 +0200

    Table component added

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,9 +1,101 @@
-import Pagination from '@/components/Pagination'
-import TableSearch from '@/components/TableSearch'
-import Image from 'next/image'
-import React from 'react'
+import Pagination from '@/components/Pagination';
+import Table from '@/components/Table';
+import TableSearch from '@/components/TableSearch';
+import { role, teachersData } from '@/lib/data';
+import Image from 'next/image';
+import Link from 'next/link';
+
+type Teacher = {
+    id: number;
+    teacherId: string;
+    name: string;
+    email?: string;
+    photo: string;
+    phone: string;
+    subjects: string[];
+    classes: string[];
+    address: string;
+  };
+
+  const columns = [
+    {
+      header: "Info",
+      accessor: "info",
+    },
+    {
+      header: "Teacher ID",
+      accessor: "teacherId",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Subjects",
+      accessor: "subjects",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Classes",
+      accessor: "classes",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Phone",
+      accessor: "phone",
+      className: "hidden lg:table-cell",
+    },
+    {
+      header: "Address",
+      accessor: "address",
+      className: "hidden lg:table-cell",
+    },
+    {
+      header: "Actions",
+      accessor: "action",
+    },
+  ];
 
 export default function TeacherList() {
+
+const renderRow = (item: Teacher) => (
+    <tr
+    className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-secondaryLight' 
+    key={item.id}
+    >
+        <td className='flex items-center gap-4 p-4'>
+            <Image 
+            src={item.photo} 
+            alt="" 
+            width={40} 
+            height={40} 
+            className='md:hidden xl:block 
+            w-10 h-10 rounded-full object-cover'
+            />
+            <div className='flex flex-col'>
+                <h3 className='font-semibold'>{item.name}</h3>
+                <p className='text-xs text-gray-500'>{item?.email}</p>
+            </div>
+        </td>
+        <td className='hidden md:table-cell'>{item.teacherId}</td>
+        <td className='hidden md:table-cell'>{item.subjects.join(",")}</td>
+        <td className='hidden md:table-cell'>{item.classes.join(",")}</td>
+        <td className='hidden md:table-cell'>{item.teacherId}</td>
+        <td className='hidden md:table-cell'>{item.teacherId}</td>
+        <td>
+            <div className='flex items-center gap-2'>
+                <Link href={`/list/teachers/${item.id}`}>
+                    <button className='w-7 h-7 flex items-center justify-center rounded-full bg-permanent'>
+                        <Image src="/view.png" alt="view" width={16} height={16}/>
+                    </button>
+                </Link>
+                {role === "admin" && (
+                    <button className='w-7 h-7 flex items-center justify-center rounded-full bg-secondary'>
+                        <Image src="/delete.png" alt="view" width={16} height={16}/>
+                    </button>
+                )}
+            </div>
+        </td>
+    </tr>
+)
+
   return (
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
         <div className='flex items-center justify-between'>
@@ -25,7 +117,7 @@ export default function TeacherList() {
         </div>
         <div className=''></div>
         {/*LIST*/}
-        
+        <Table columns={columns} renderRow={renderRow} data={teachersData}/>
         {/*PAGINATION*/}
         <Pagination/>
     </div>
