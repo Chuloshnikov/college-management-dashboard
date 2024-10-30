be2f117837069f20c11e1225695595d92baa4624
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-10-30 16:01:53 +0200

    form component added

---
+++ src/components/forms/ClassForm.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+
+
+const ClassForm = () => {
+  return (
+    <div>ClassForm</div>
+  )
+}
+
+export default ClassForm;
---
+++ src/components/forms/ExamForm.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+
+const ExamForm = () => {
+  return (
+    <div>ExamForm</div>
+  )
+}
+
+export default ExamForm
